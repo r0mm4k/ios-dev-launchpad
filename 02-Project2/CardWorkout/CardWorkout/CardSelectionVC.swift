d315a68d700e33ef7f752ed39a66bb95c2907453
Roma Makarov <r0mmm4k@gmail.com>
2021-05-19 19:46:21 +0300

    add logic to project 2

--- 02-Project2/CardWorkout/CardWorkout/CardSelectionVC.swift
+++ 02-Project2/CardWorkout/CardWorkout/CardSelectionVC.swift
@@ -12,18 +12,40 @@ class CardSelectionVC: UIViewController {
     @IBOutlet var cardImageView: UIImageView!
     @IBOutlet var buttons: [UIButton]!
     
+    var timer: Timer!
+    var cards: [UIImage] = CardDeck.allValues
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        startTimer()
 
         for button in buttons {
             button.layer.cornerRadius = 8
         }
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        timer.invalidate()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
 
     @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
     }
     
     @IBAction func restartButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
     }
     
     @IBAction func rulesButtonTapped(_ sender: UIButton) {
