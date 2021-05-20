6b8dfcaa6c4fabe769e961bf11a186cac5febeff
Roma Makarov <r0mmm4k@gmail.com>
2021-05-20 11:32:54 +0300

    Scene & Button settings

---
+++ 03-Project3/CardWorkoutProgrammatic/CardWorkoutProgrammatic/CWButton.swift
@@ -0,0 +1,36 @@
+//
+//  CWButton.swift
+//  CardWorkoutProgrammatic
+//
+//  Created by Роман Макаров on 20.05.21.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+    }
+    
+    func configure() {
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
---
+++ 03-Project3/CardWorkoutProgrammatic/CardWorkoutProgrammatic/CardSelectionVC.swift
@@ -0,0 +1,18 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkoutProgrammatic
+//
+//  Created by Роман Макаров on 20.05.21.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
